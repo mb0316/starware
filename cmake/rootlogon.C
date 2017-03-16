6739e05e214ddd36870c538050bccbb8572472c5
mb0316 <MoonByul@munbyeol-ui-MacBook-Pro.local>
2017-03-16 12:58:15 +0900

    beta ver.1.2

--- cmake/rootlogon.C
+++ cmake/rootlogon.C
@@ -2,9 +2,7 @@
 	TString lib = TString(gSystem -> pwd()) + "/libSTARWARE.dylib";
 	cout << lib << endl;
 	if (gSystem -> Load(lib) != -1)
-	{
 		cout << "STAWARE system has been successfully loaded." << endl;
-	}
 	else
 		cout << "Cannot load BMspec" << endl;
 }
