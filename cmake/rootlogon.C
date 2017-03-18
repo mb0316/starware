f66c1154a9ba8a318c29f25081a525fc31ae899c
mb0316 <MoonByul@munbyeol-ui-MacBook-Pro.local>
2017-03-18 17:34:12 +0900

    beta ver.1.2

--- cmake/rootlogon.C
+++ cmake/rootlogon.C
@@ -1,7 +1,9 @@
 {
-	TString lib = TString(gSystem -> pwd()) + "/libSTARWARE.dylib";
-	cout << lib << endl;
-	if (gSystem -> Load(lib) != -1)
+	TString lib_MacOS = TString(gSystem -> pwd()) + "/libSTARWARE.dylib";
+	TString lib_Linux = TString(gSystem -> pwd()) + "/libSTARWARE.so";
+	if (gSystem -> Load(lib_MacOS) != -1)
+		cout << "STAWARE system has been successfully loaded." << endl;
+	else if (gSystem -> Load(lib_Linux) != -1)
 		cout << "STAWARE system has been successfully loaded." << endl;
 	else
 		cout << "Fail to load STARWARE." << endl;
